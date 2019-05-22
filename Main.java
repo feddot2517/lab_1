99dbd6cb2fab00d79797a6b5d72d985afe8005a2
feddot2517 <feddot2517@gmail.com>
2019-05-23 05:48:12 +0600

    a

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "aaabbbcccddd";
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
         String t="";
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
